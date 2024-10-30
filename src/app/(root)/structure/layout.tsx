7b2ef3360be59ef54539befb4454ed8d8d1518af
hircent <hircent@gmail.com>
2024-10-30 11:46:22 +0800

    Category layout is done

--- src/app/(root)/structure/layout.tsx
+++ src/app/(root)/structure/layout.tsx
@@ -1,3 +1,4 @@
+import CreateCategory from '@/components/CreateCategory'
 import SearchBar from '@/components/SearchBar'
 import StructureNav from '@/components/StructureNav'
 import { Search } from 'lucide-react'
@@ -8,7 +9,10 @@ const Layout = ({children}:{children:React.ReactNode}) => {
   return (
     <section className="home">
         <div className="home-content">
-            <SearchBar/>
+            <div className="flex justify-between">
+              <SearchBar />
+              <CreateCategory type={"branch"} />
+            </div>
             <div className='rounded-md border bg-yellow-2 text-gray-500 p-2 px-4'>
                 <StructureNav/>
                 {children}
---
+++ src/components/CreateCategory.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { CreateType } from "@/types/index";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogOverlay,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { useEffect, useState } from "react";
+import { Plus } from "lucide-react";
+import { useToast } from "./ui/use-toast";
+import { camelCase, cn } from "@/lib/utils";
+
+
+const CreateCategory = (params: CreateType) => {
+  const { type } = params;
+  const [open, setOpen] = useState<boolean>(false);
+  const { toast } = useToast();
+
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          onClick={() => {
+            setOpen(true);
+          }}
+          className="group p-2 bg-gray-100 rounded-md hover:bg-yellow-2"
+        >
+          <Plus size={18} className="text-red-600 group-hover:text-gray-600" />{" "}
+          Add
+        </Button>
+      </DialogTrigger>
+      <DialogOverlay
+        className="bg-black/80"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.8)" }}
+      />
+      <DialogContent className="w-full max-w-[1000px] h-[90vh] overflow-y-auto custom-scrollbar bg-white">
+        <DialogHeader>
+          <DialogTitle className="text-xl sm:text-2xl font-semibold">
+            Create Branch
+          </DialogTitle>
+          <DialogDescription className="text-sm sm:text-base">
+            Click create when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateCategory;
