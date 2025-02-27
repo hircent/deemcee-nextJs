94d2f1168739ea60007eb2ab93a0def01b86c185
hircent <hircent@gmail.com>
2025-02-27 15:19:12 +0800

    enrolment page preparation

--- src/app/(root)/deusers/enrolment/page.tsx
+++ src/app/(root)/deusers/enrolment/page.tsx
@@ -1,7 +1,37 @@
+import { PageListTable } from "@/components/PageList";
+import { PARENT } from "@/constants/message";
 import React from "react";
+import { SearchParamProps } from "@/types/index";
+import Pagination from "@/components/Pagination";
+import { EnrolmentListColumns } from "@/columns/enrolment.list.columns";
+import { getEnrolmentList } from "@/lib/actions/student.action";
 
-const Enrolment = () => {
-  return <div>Enrolment</div>;
+const Enrolment = async ({ searchParams }: SearchParamProps) => {
+  try {
+    const result = await getEnrolmentList({
+      name: searchParams.q ? searchParams.q.toString() : undefined,
+      is_active: searchParams.is_active
+        ? searchParams.is_active === "true"
+          ? true
+          : false
+        : true,
+      status: searchParams.status ? searchParams.status.toString() : "KIDS",
+    });
+    console.log(result);
+    return (
+      <div className="p-4">
+        <PageListTable columns={EnrolmentListColumns} data={result.data} />
+
+        <Pagination
+          next={result.next}
+          previous={result.previous}
+          baseUrl="/deusers/enrolment"
+        />
+      </div>
+    );
+  } catch (error) {
+    return;
+  }
 };
 
 export default Enrolment;
