7422d781d6eacb7ba8c5ec7f8f88a0de2b95b17c
hircent <hircent@gmail.com>
2025-03-05 19:20:34 +0800

    pass country prop into CreateDeUsers component

--- src/app/(root)/deusers/layout.tsx
+++ src/app/(root)/deusers/layout.tsx
@@ -5,7 +5,7 @@ import SectionNav from "@/components/SectionNav";
 import StudentFilter from "@/components/StudentFilter";
 import { DeUsersLinks, IsManagerOrHigher } from "@/constants/index";
 import { authUser } from "@/lib/actions/user.actions";
-import { getUserRole } from "@/lib/utils";
+import { getBranchCountry, getUserRole } from "@/lib/utils";
 import { cookies } from "next/headers";
 import React from "react";
 
@@ -13,13 +13,17 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
   const user = await authUser();
   const userRole = getUserRole(user);
   const branchId = cookies().get("BranchId")?.value;
+  const country = getBranchCountry(+branchId!, user!.branch_role);
+
   return (
     <div className="home-content">
       <div className="flex flex-wrap justify-between gap-2">
         <SearchBar />
         <StudentFilter />
         <ExportStudents id={branchId!.toString()} />
-        {IsManagerOrHigher.includes(userRole[0]) && <CreateDeUsers />}
+        {IsManagerOrHigher.includes(userRole[0]) && (
+          <CreateDeUsers country={country} />
+        )}
       </div>
       <div className="rounded-md border bg-yellow-2 text-gray-500 p-2 px-4">
         <SectionNav links={DeUsersLinks} />
