dbb1fee718759e374dda364d585569cd6c34c304
hircent <hircent@gmail.com>
2025-03-08 12:05:54 +0800

    Add read and printed page for certificate

---
+++ src/app/(root)/certificate/layout.tsx
@@ -0,0 +1,25 @@
+import SearchBar from "@/components/SearchBar";
+import SectionNav from "@/components/SectionNav";
+import { CertificateLinks } from "@/constants/index";
+import { authUser } from "@/lib/actions/user.actions";
+import { getUserRole } from "@/lib/utils";
+import React from "react";
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const user = await authUser();
+  const userRole = getUserRole(user);
+
+  return (
+    <div className="home-content">
+      <div className="flex flex-wrap justify-between gap-2">
+        <SearchBar />
+      </div>
+      <div className="rounded-md border bg-yellow-2 text-gray-500 p-2 px-4">
+        <SectionNav links={CertificateLinks} />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
