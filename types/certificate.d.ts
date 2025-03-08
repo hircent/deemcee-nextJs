c3371b61c1169bbfb9209dcd398025d19718c374
hircent <hircent@gmail.com>
2025-03-08 12:31:55 +0800

    CertificateData type added

---
+++ types/certificate.d.ts
@@ -0,0 +1,15 @@
+export type CertificateData = {
+  id: number;
+  student: {
+    id: number;
+    first_name: string;
+    last_name: string;
+    fullname: string;
+  };
+  grade: number;
+  start_date: string;
+  end_date: string;
+  status: string;
+  is_printed: boolean;
+  branch: number;
+};
