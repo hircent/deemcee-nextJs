33d40396c0596a580af34665fece79f2aedf271a
hircent <hircent@gmail.com>
2025-03-08 12:31:36 +0800

    loading page added

---
+++ src/app/(root)/certificate/ready/loading.tsx
@@ -0,0 +1,12 @@
+import Loader from "@/components/Loader";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center mt-8">
+      <Loader />
+    </div>
+  );
+};
+
+export default Loading;
