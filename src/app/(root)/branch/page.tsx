42edfb93703224e9f69756709193cdeeb6871e0d
hircent <hircent@gmail.com>
2025-03-09 14:31:27 +0800

    remove trycatch block

--- src/app/(root)/branch/page.tsx
+++ src/app/(root)/branch/page.tsx
@@ -2,44 +2,38 @@ import { BranchListColumns } from "@/columns/branch.list.columns";
 import { PageListTable } from "@/components/PageList";
 import CreateBranch from "@/components/CreateBranch";
 import SearchBar from "@/components/SearchBar";
-import { Button } from "@/components/ui/button";
 import { BRANCH } from "@/constants/message";
 import { getBranchList } from "@/lib/actions/branch.action";
 import { authUser } from "@/lib/actions/user.actions";
 import { getUserRole } from "@/lib/utils";
 import { BranchProps, ListProps, SearchParamProps } from "@/types/index";
-import Link from "next/link";
 
 import React from "react";
 import { IsSuperadmin } from "@/constants/index";
 import Pagination from "@/components/Pagination";
 
 export default async function Branch({ searchParams }: SearchParamProps) {
-  try {
-    const user = await authUser();
-    const userRole = getUserRole(user);
-    let result: ListProps<BranchProps>;
-    result = await getBranchList({
-      page: searchParams.page ? +searchParams.page : 1,
-      searchQuery: searchParams.q ? searchParams.q.toString() : undefined,
-    });
+  const user = await authUser();
+  const userRole = getUserRole(user);
+  let result: ListProps<BranchProps>;
+  result = await getBranchList({
+    page: searchParams.page ? +searchParams.page : 1,
+    searchQuery: searchParams.q ? searchParams.q.toString() : undefined,
+  });
 
-    return (
-      <div className="home-content">
-        <div className="flex justify-between">
-          <SearchBar />
-          {IsSuperadmin.includes(userRole[0]) && <CreateBranch type={BRANCH} />}
-        </div>
-        <PageListTable columns={BranchListColumns} data={result.data} />
-
-        <Pagination
-          next={result.next}
-          previous={result.previous}
-          baseUrl="/branch"
-        />
+  return (
+    <div className="home-content">
+      <div className="flex justify-between">
+        <SearchBar />
+        {IsSuperadmin.includes(userRole[0]) && <CreateBranch type={BRANCH} />}
       </div>
-    );
-  } catch (error) {
-    return;
-  }
+      <PageListTable columns={BranchListColumns} data={result.data} />
+
+      <Pagination
+        next={result.next}
+        previous={result.previous}
+        baseUrl="/branch"
+      />
+    </div>
+  );
 }
