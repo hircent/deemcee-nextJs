446e257dcf9d446f19ad95d6463830356a8d8c2e
hircent <hircent@gmail.com>
2025-03-09 14:35:25 +0800

    page and q passed into params

--- lib/actions/certificate.action.ts
+++ lib/actions/certificate.action.ts
@@ -1,22 +1,33 @@
 "use server";
 
-import { CertificateData } from "@/types/certificate";
+import { CertificateData, CertificateParams } from "@/types/certificate";
 import { ListProps } from "@/types/index";
 import { cookies } from "next/headers";
 import { getToken } from "./user.actions";
 
 export const getCertificateList = async (
-  isPrinted: boolean = false
+  params: CertificateParams
 ): Promise<ListProps<CertificateData>> => {
+  const { isPrinted, page, q } = params;
   try {
     const token = await getToken();
     const branchId = cookies().get("BranchId")?.value;
-    let url = `${process.env.API_URL}/certificate/list`;
-    if (isPrinted) {
-      url = `${process.env.API_URL}/certificate/list?is_printed=1`;
+    const url = new URL(`${process.env.API_URL}/certificate/list`);
+    const searchParams = new URLSearchParams();
+
+    searchParams.set("is_printed", isPrinted ? "1" : "2");
+
+    if (page && page > 1) {
+      searchParams.set("page", page.toString());
     }
 
-    const response = await fetch(url, {
+    if (q) {
+      searchParams.set("q", q);
+    }
+
+    url.search = searchParams.toString();
+
+    const response = await fetch(url.toString(), {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -26,7 +37,6 @@ export const getCertificateList = async (
     });
 
     const data = await response.json();
-
     return data;
   } catch (error) {
     throw new Error("Failed to fetch certificate data " + error);
