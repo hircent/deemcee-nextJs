e8ab870881cad611c2171c9d974e15af45bb18c5
hircent <hircent@gmail.com>
2025-03-09 14:34:22 +0800

    add searchbar

--- src/app/(root)/certificate/layout.tsx
+++ src/app/(root)/certificate/layout.tsx
@@ -1,3 +1,4 @@
+import SearchBar from "@/components/SearchBar";
 import SectionNav from "@/components/SectionNav";
 import { CertificateLinks } from "@/constants/index";
 import React from "react";
@@ -5,6 +6,10 @@ import React from "react";
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="home-content">
+      <div className="flex flex-wrap justify-between gap-2">
+        <SearchBar />
+        <div></div>
+      </div>
       <div className="rounded-md border bg-yellow-2 text-gray-500 p-2 px-4">
         <SectionNav links={CertificateLinks} />
         {children}
