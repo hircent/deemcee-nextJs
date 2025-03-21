d37351d37a37bcff4f59c5e0b6eee8803783272e
hircent <hircent@gmail.com>
2025-03-21 15:42:17 +0800

    getPaymentReportList added

--- lib/actions/payment.action.ts
+++ lib/actions/payment.action.ts
@@ -2,7 +2,11 @@
 
 import { getToken } from "./user.actions";
 import { cookies } from "next/headers";
-import { PaymentData } from "@/types/payment";
+import {
+  PaymentData,
+  PaymentReportData,
+  PaymentReportParams,
+} from "@/types/payment";
 
 export async function getPaymentDetails(id: number): Promise<PaymentData> {
   const token = await getToken();
@@ -20,6 +24,33 @@ export async function getPaymentDetails(id: number): Promise<PaymentData> {
         },
       }
     );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch payment data " + response.statusText);
+    }
+
+    const data = await response.json();
+
+    return data.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getPaymentReportList(
+  param: PaymentReportParams
+): Promise<PaymentReportData[]> {
+  try {
+    const token = await getToken();
+    const branchId = cookies().get("BranchId")?.value;
+    const response = await fetch(`${process.env.API_URL}/payment-report/list`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token?.value}`,
+        BranchId: `${branchId?.toString()}`,
+      },
+    });
 
     if (!response.ok) {
       throw new Error("Failed to fetch payment data " + response.statusText);
