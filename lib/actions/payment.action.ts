244e31642cc033c77769e4ae50f8432af69a26e1
hircent <hircent@gmail.com>
2025-03-27 15:24:04 +0800

    makePayment added

--- lib/actions/payment.action.ts
+++ lib/actions/payment.action.ts
@@ -3,10 +3,13 @@
 import { getToken } from "./user.actions";
 import { cookies } from "next/headers";
 import {
+  MakePaymentFormErrors,
   PaymentData,
   PaymentReportData,
   PaymentReportParams,
 } from "@/types/payment";
+import { STATE } from "@/types/index";
+import { MakePaymentSchema } from "@/constants/form";
 
 export async function getPaymentDetails(id: number): Promise<PaymentData> {
   const token = await getToken();
@@ -66,5 +69,47 @@ export async function getPaymentReportList(
     return data.data;
   } catch (error) {
     throw error;
+  }
+}
+
+export async function makePayment(
+  _prevState: STATE<MakePaymentFormErrors>,
+  formData: FormData
+): Promise<STATE<MakePaymentFormErrors>> {
+  try {
+    const token = await getToken();
+    const branchId = cookies().get("BranchId")?.value;
+
+    const data = Object.fromEntries(formData);
+    const validated = MakePaymentSchema.safeParse(data);
+    if (!validated.success) {
+      return {
+        error: true,
+        zodErr: validated.error.flatten().fieldErrors as MakePaymentFormErrors,
+        msg: "Validation Failed",
+      };
+    }
+
+    const response = await fetch(
+      `${process.env.API_URL}/payment/make-payment/${data.id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token?.value}`,
+          BranchId: `${branchId?.toString()}`,
+        },
+        body: JSON.stringify(data),
+      }
+    );
+
+    if (!response.ok) {
+      const res = await response.json();
+      return { success: false, msg: response.statusText };
+    }
+
+    return { success: true, msg: "Payment is made successfully" };
+  } catch (error) {
+    return { error: true, msg: (error as Error).message };
   }
 }
