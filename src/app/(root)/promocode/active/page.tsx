623326b222c042b186507cbfa809de272311d6c2
hircent <hircent@gmail.com>
2025-03-31 21:28:46 +0800

    PaginationWrapper reuse

--- src/app/(root)/promocode/active/page.tsx
+++ src/app/(root)/promocode/active/page.tsx
@@ -3,17 +3,16 @@ import { PageListTable } from "@/components/PageList";
 import { PromoCodeListColumns } from "@/columns/promocode.list.column";
 import Pagination from "@/components/Pagination";
 import { getPromoCodeList } from "@/lib/actions/promocode.action";
+import { PaginationWrapper } from "@/components/PaginationWrapper";
 
 const Active = async () => {
   const result = await getPromoCodeList();
 
   return (
     <div className="p-4">
-      <PageListTable columns={PromoCodeListColumns} data={result.data} />
-
-      <Pagination
-        next={result.next}
-        previous={result.previous}
+      <PaginationWrapper
+        columns={PromoCodeListColumns}
+        paginationData={result}
         baseUrl="/promocode"
       />
     </div>
