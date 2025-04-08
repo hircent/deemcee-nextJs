f7efdeb06c5b7e07f93e93a071544481c3bdd054
hircent <hircent@gmail.com>
2025-04-08 21:29:43 +0800

    use force-dynamic

--- src/app/(root)/promocode/inactive/page.tsx
+++ src/app/(root)/promocode/inactive/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { PageListTable } from "@/components/PageList";
 import { PromoCodeListColumns } from "@/columns/promocode.list.column";
-import Pagination from "@/components/Pagination";
 import { getPromoCodeList } from "@/lib/actions/promocode.action";
 import { PaginationWrapper } from "@/components/PaginationWrapper";
+
+export const dynamic = "force-dynamic";
 
 const Inactive = async () => {
   const result = await getPromoCodeList();
